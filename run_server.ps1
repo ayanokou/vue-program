776d277ffc0666807f4918e2e5a509ff1f05e0d4
orokane <s1ran@outlook.com>
2023-07-04 16:47:01 +0800

    ...

--- run_server.ps1
+++ run_server.ps1
@@ -1,4 +1,6 @@
 Set-Location .\server
-mvn -D'maven.test.skip=true' package
+# mvn -D'maven.test.skip=true' package
+mvn package
 Set-Location ..
-java -D'java.library.path=server/src/main/resources' -jar server/target/demo-0.0.1-SNAPSHOT.jar
+# java -D'java.library.path=server/src/main/resources' -jar server/target/demo-0.0.1-SNAPSHOT.jar
+java -jar server/target/demo-0.0.1-SNAPSHOT-shaded.jar
